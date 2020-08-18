ca237c5827b968be81630f7103ee6d8ba6551b26
Serkan Sahin <ssahin55@outlook.com>
2020-08-18 19:38:56 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,28 +1,21 @@
 def addition(num1, num2)
-  sum = num2 + num1
-  return sum
+  num2 + num1
 end
 
 def subtraction(num1, num2)
-  sub = num1 - num2
-  return sub
+  num1 - num2
 end
 
 def division(num1, num2)
-  div = num1 / num2
-  return div
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-  mult = num1 * num2
-  return mult
+  num1 * num2
 end
 
 def modulo(num1, num2)
   num1 % num2
 end
 
-def square_root(num)
-  square = num * num
-  return square
-end
+#

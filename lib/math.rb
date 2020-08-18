77e7b1cd53f4b517b79b23672b10bfd2e966f665
Serkan Sahin <ssahin55@outlook.com>
2020-08-18 19:40:06 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,18 +4,16 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  sub = num1 - num2
-  return sub
+  num1 - num2
 end
 
 def division(num1, num2)
-  div = num1 / num2
-  return div
+  num1 / num2
+
 end
 
 def multiplication(num1, num2)
   num1 * num2
-
 end
 
 def modulo(num1, num2)
